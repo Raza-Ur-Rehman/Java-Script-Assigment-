45760060b01e40976a1407506a3be3b649ead86e
Raza ur Rehman <121747444+Raza-Ur-Rehman@users.noreply.github.com>
2023-01-27 12:15:09 -0800

    java script first assignment

---
+++ app.js
@@ -0,0 +1,27 @@
+// chapter 1  //
+
+// Question no 1
+
+alert("HELLO USER")
+
+// Question no 2
+
+alert("Erorr! enter a invalid input")
+
+// Question no 3
+
+alert("Welcome to JS World\nHappy coding")
+
+// Question no 4
+
+alert("Welcome to JS World")
+
+// Question no 5
+
+alert("Hello! i can run JS through my web browser console")
+
+// Question no 6
+
+alert("Using alert in My New Html Project")
+
+// chapter 1 Assignment closed //
